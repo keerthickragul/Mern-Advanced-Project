dc46b20bf6dbd1d6a81e16c25edb6c993dd08a5c
Keerthick_ragul <125256655+keerthickragul@users.noreply.github.com>
2024-08-08 10:39:12 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,8 @@ const express =require ("express")
 const Router = express.Router()
 const productController = require("../controllers/productController.js")
 const {auth}= require("../middleware/auth.js")
-Router.get("/get",auth,productController.getAllProducts);
+// Router.get("/get",auth,productController.getAllProducts);
+Router.get("/get",productController.getAllProducts);
 Router.post("/add",productController.createProducts);
 Router.patch("/update/:id", productController.updateProduct);
 Router.delete("/delete/:id",productController.deleteProduct)
